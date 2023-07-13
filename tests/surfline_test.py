0a898b05785123ff73bc10b8c593564e09ac93bd
Sean Muirhead <smmuirhead100@berkeley.edu>
2023-07-13 09:58:29 -0700

    implement unit tests and fix relative imports

---
+++ tests/surfline_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+# Add directories to current path
+import sys
+sys.path.append('../')
+sys.path.append('../surfline')
+
+# Import methods to test
+from surfline.surfline_data import getTides
+
+#----------------------#
+#----- Unit Tests -----#
+
+# Test surfline_data.py methods
+class TestSurflineMethods(unittest.TestCase):
+    
+    # Test getTides() method. Make sure it is a list, and is not empty.
+    def testGetTides(self):
+        tides = (getTides('5842041f4e65fad6a7708a7a'))
+        self.assertEqual(type(tides), type([]))
+        self.assertGreaterEqual(len(tides), 1)
+        
+if __name__ == '__main__':
+    unittest.main()
