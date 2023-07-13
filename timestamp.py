4ffb351b2f56ed0e52d9abea06f8124ce3117f24
Sean Muirhead <smmuirhead100@berkeley.edu>
2023-07-13 06:51:38 -0700

    implement timestamp.unixToHuman()

---
+++ timestamp.py
@@ -0,0 +1,14 @@
+import datetime
+
+# Converts a Unix timestamp to human-readable format. 
+# Ex. 1609459200 -> 2021-01-01 00:00:00
+
+def unixToHuman(timestamp : int) -> str:
+
+    # Convert the timestamp to a datetime object
+    datetime_object = datetime.datetime.fromtimestamp(timestamp)
+
+    # Format the datetime object as a string
+    formatted_date = datetime_object.strftime("%Y-%m-%d %H:%M:%S")
+
+    return formatted_date
