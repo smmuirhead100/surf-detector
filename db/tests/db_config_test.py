86f9c3bd12b8b97e92933ef375d27c5cdfc21f75
Sean Muirhead <smmuirhead100@berkeley.edu>
2023-07-16 08:23:55 -0700

    complete config test for SLConfig

--- db/tests/db_config_test.py
+++ db/tests/db_config_test.py
@@ -14,7 +14,7 @@ from db_config import SurflineTypes
 #----- Unit Tests -----#
 
 # Test database Surfline Configuration. addTypes and addTables are commented because they are only needed to be run once.
-class TestDatabaseMethods(unittest.TestCase):
+class TestSurflineConfig(unittest.TestCase):
     
     @classmethod
     def setUpClass(cls):
@@ -29,10 +29,10 @@ class TestDatabaseMethods(unittest.TestCase):
         result = self.surfData.addTables()
         self.assertTrue(result)
 
-    # # Test insert tide data
-    # def testInsertTide(self):
-    #     self.db.insert("tide", {"timestamp": 123456789, "utcOffset": 0, "type": "HIGH", "height": 1.2})
-    #     self.db.insert("tide", {"timestamp": 123456790, "utcOffset": 0, "type": "LOW", "height": 0.2})
+    # Test insert tide data
+    def testInsertTide(self):
+        self.db.insert("tide", {"timestamp": 123456789, "utcOffset": 0, "type": "HIGH", "height": 1.2})
+        self.db.insert("tide", {"timestamp": 123456790, "utcOffset": 0, "type": "LOW", "height": 0.2})
     
 
     # Test insert wave data
@@ -43,7 +43,20 @@ class TestDatabaseMethods(unittest.TestCase):
         
         swells = [{"height": 1.2, "period": 12.3, "impact": 1.2, "power": 1.2, "direction": 1.2, "directionMin": 1.2, "optimalScore": 1}, {"height": 1.2, "period": 12.3, "impact": 1.2, "power": 1.2, "direction": 1.2, "directionMin": 1.2, "optimalScore": 1}]
         
-        self.db.insert("wave", {"timestamp": 12949433, "probability": 0, "utcOffset": -4, "surf": surf, "power": 1.2, "swell": swells[0]})
+    # Test insert wind data
+    def testInsertWind(self):
+        wind = {"timestamp": 123456789, "utcOffset": 0, "speed": 1.2, "direction": 1.2, "directionType": "N", "gust": 1.2, "optimalScore": 1}
+        self.db.insert("wind", wind)
+    
+    # Test insert weather data
+    def testInsertWeather(self):
+        weather = {"timestamp": 123456789, "utcOffset": 0, "temperature": 1.2, "condition": "SUNNY", "pressure": 1.2}
+        self.db.insert("weather", weather)
+        
+    # Test insert rating data
+    def testInsertRating(self):
+        rating = {"timestamp": 123456789, "utcOffset": 0, "rating": {"key": "test", "value": 1}}
+        self.db.insert("rating", rating)
     
     # Test get tide data
     def testGetTides(self):
@@ -56,6 +69,24 @@ class TestDatabaseMethods(unittest.TestCase):
         for row in result:
             time = row[0]
             print(time + 10) # To make sure what we are getting is actually an integer
+    
+    # Test get wind data
+    def testGetWinds(self):
+        result = self.db.getTable("wind")
+        for row in result:
+            print(row)
+            
+    # Test get weather data
+    def testGetWeather(self):
+        result = self.db.getTable("weather")
+        for row in result:
+            print(row)
+            
+    # Test get rating data
+    def testGetRating(self):
+        result = self.db.getTable("rating")
+        for row in result:
+            print(row)
             
 if __name__ == '__main__':
     unittest.main()
