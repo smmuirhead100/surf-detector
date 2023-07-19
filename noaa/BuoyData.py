a161e3b08e44420b13834b0a993b4da5aa4b212e
Sean Muirhead <smmuirhead100@berkeley.edu>
2023-07-19 06:49:19 -0700

    add keys to weatherData

--- noaa/BuoyData.py
+++ noaa/BuoyData.py
@@ -11,17 +11,19 @@ from noaa.Weather import getWeatherData
 # Get all weather and swell data for a given location. Location ID refers to the NOAA buoy ID. Location name refers to the name of the location (ex. Huntington Beach, CA)
 # Ex. getAllData('46222', 'Huntington Beach')
 
-def getAllData(locationID, locationName):
+def getAllData(buoyId, spotName):
    
     # Retrieve data from different APIs
-    swellData = getSwellData(locationID)
-    weatherData = getWeatherData(locationName)
+    swellData = getSwellData(buoyId)
+    weatherData = getWeatherData(spotName)
     
     # Return a dictionary with all data
     
     returnDict = {}
     
     returnDict['time'] = time.time()
+    returnDict['buoyID'] = buoyId
+    returnDict['spotName'] = spotName
     returnDict['Swell'] = swellData
     returnDict['Weather'] = weatherData
     
