e4bbdacba509debc442b2a9e9164303b1af4f526
Sean Muirhead <smmuirhead100@berkeley.edu>
2023-07-30 10:27:17 -0400

    update navbar headers

--- frontend/app/src/components/generalNavbar.tsx
+++ frontend/app/src/components/generalNavbar.tsx
@@ -1,18 +1,57 @@
 import './style/generalNavbar.css'
 import logo from '../assets/logo.svg'
+import plus from '../assets/plus.svg'
+import minus from '../assets/minus.svg'
+import { useState } from 'react'
 
 export default function GeneralNavbar() {
+    const [spotsOpen, setSpotsOpen] = useState(false)
+    const [camsOpen, setCamsOpen] = useState(false)
+    
+    function handleSpotsClick(){
+        setSpotsOpen(prev => !prev)
+    }
+
+    function handleCamsClick(){
+        setCamsOpen(prev => !prev)
+    }
+
     return (
         <div className="general--navbar">
             <div className="general--navbar--logo">
                 <img src={logo} alt="logo" />
             </div>
             <ul className="navbar--list">
-                <li>Spots</li>
-                <li>Cams</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Contribute</li>
+                <li onClick={handleSpotsClick}>Spots
+                    <img src={spotsOpen ? minus : plus} />
+                </li>
+                {spotsOpen ?                                        // Render Spots if is open
+                    <div className='navbar--dropdown'>
+                        <li>San Diego</li>
+                        <li>Huntington Beach</li>
+                        <li>Newport Beach</li>
+                        <li>Malibu</li>
+                        <li>Ventura</li>
+                    </div> : null
+                }
+                
+                <li onClick={handleCamsClick}>Cams
+                    <img src={camsOpen ? minus : plus} />
+                </li>
+                {camsOpen ?                                        // Render Cams if is open
+                    <div className='navbar--dropdown'>
+                        <li>Coming soon!</li>
+                    </div> : null
+                }
+
+                <li>About
+                </li>
+
+                <li>Contact
+                </li>
+
+                <li>Contribute
+                </li>
             </ul>
         </div>
     )
