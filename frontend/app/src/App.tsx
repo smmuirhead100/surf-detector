66790a62579fbb18acd5c99902ab3031cd8e7064
Sean Muirhead <smmuirhead100@berkeley.edu>
2023-08-06 19:26:06 -0700

    add landing page

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,11 +1,13 @@
 import SpotForecast from "./components/spotForecast"
+import Home from "./components/home"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<SpotForecast spot='huntington_beach'/>}>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='forecast' element={<SpotForecast spot='huntington_beach'/>}>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/app/src/components/home.tsx
@@ -0,0 +1,34 @@
+import './style/home.css'
+import logo from '../assets/logo.svg'
+import contactIconBlack from '../assets/contactIconBlack.svg'
+
+export default function Home() {
+    return (
+        <div className="home--wrapper">
+            <div className="home--left">
+
+            </div>
+            <div className="home--right">
+                
+                <div className="navbar">
+                    <li>About</li>
+                    <div className='navbar--contact'>
+                        <img src={contactIconBlack} alt='Contact Icon'/>
+                        <h3>Contact</h3>
+                    </div>
+        
+                </div>
+
+                <div className="content">
+                    <img src={logo}/>
+                    <h2>SCORE MORE BARRELS.</h2>
+                </div>
+
+                <div className="getStarted">
+                    <button>Get Started</button>
+                    <div>Or log in</div>
+                </div>
+            </div>
+        </div>
+    )
+}
