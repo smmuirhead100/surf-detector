73d7efc497841d16c889e1f802d6ce8ac36956db
Sean Muirhead <smmuirhead100@berkeley.edu>
2023-08-24 10:52:54 -0700

    update navbar display

--- frontend/app/src/pages/spot_forecast/generalNavbar.tsx
+++ frontend/app/src/pages/spot_forecast/generalNavbar.tsx
@@ -7,40 +7,40 @@ import cameraIconWhite from '../../assets/cameraIconWhite.svg'
 import contactIconBlack from '../../assets/contactIconBlack.svg'
 import aboutIconBlack from '../../assets/aboutIconBlack.svg'
 import githubIconBlack from '../../assets/githubIcon.png'
-import navbarLine from '../../assets/navbarLine.svg'
 import logoutIcon from '../../assets/logoutIcon.svg'
 import { useState, useEffect } from 'react'
 import getSpots from '../../utils/getSpots'
 import { useAuth } from "../../context/AuthProvider"
 import { useNavigate } from "react-router-dom"
+import capitalizeAfterUnderscore from '../../utils/capitalizeAfterUnderscore'
 
 export default function GeneralNavbar(props) {
     const [spotsOpen, setSpotsOpen] = useState(false)
     const [camsOpen, setCamsOpen] = useState(false)
     const [spots, setSpots] = useState([])
+    const [dropdownX, setDropdownX] = useState(null)
+    const [dropdownY, setDropdownY] = useState(null)
     const auth = useAuth()['auth'] 
     const signOut = useAuth()['signOut']
     const navigate = useNavigate()
     
     useEffect(() => {
+        const handleWindowResize = () => {
+            setCamsOpen(false);
+            setSpotsOpen(false);
+        };
+        
+        window.addEventListener('resize', handleWindowResize);
+    
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
+    useEffect(() => {
         getSpots()
         .then(data => setSpots(data))
     },[])
-
-    // Just capitalizes the first letter of the spot, since they are typically lower case. 
-    const capitalizeAfterUnderscore = (str) => {
-        if (!str) return ''; // If the input string is empty or undefined, return an empty string
-      
-        const words = str.split('_'); // Split the string into words using underscores as separators
-      
-        // Capitalize the first letter of each word and concatenate them
-        const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
-      
-        // Join the capitalized words with spaces
-        const capitalizedString = capitalizedWords.join(' ');
-      
-        return capitalizedString;
-      };
     
     const handleLogout = async (e) => {
         console.log(auth)
@@ -64,8 +64,11 @@ export default function GeneralNavbar(props) {
         const underscoredString = words.map((word) => word.toLowerCase()).join('_');
         return underscoredString;
     };
-      
-    function handleSpotsClick(){
+    console.log(dropdownX, dropdownY)
+    function handleSpotsClick(e){
+        setDropdownX(e.currentTarget.getBoundingClientRect().left)
+        setDropdownY(e.currentTarget.getBoundingClientRect().bottom)
+        setCamsOpen(false)
         setSpotsOpen(prev => !prev)
     }
 
@@ -73,27 +76,34 @@ export default function GeneralNavbar(props) {
         props.changeSpot(revertToUnderscore(e.target.innerHTML))
     }
 
-    function handleCamsClick(){
+    function handleCamsClick(e){
+        setDropdownX(e.currentTarget.getBoundingClientRect().left)
+        setDropdownY(e.currentTarget.getBoundingClientRect().bottom)
+        setSpotsOpen(false)
         setCamsOpen(prev => !prev)
+    }
+
+    function handleLeave(){
+        setSpotsOpen(false)
+        setCamsOpen(false)
     }
 
     return (
         <div className="general--navbar">
             
             <div className="general--navbar--logo">
-                <img src={logo} alt="logo" />
+                <img src={logo} alt="logo" style={{ width: '5rem' }}/>
             </div>
 
             <ul className="navbar--list">
-                <li className={spotsOpen ? 'navbar--selected--item' : 'navbar--unselected--item'} onClick={handleSpotsClick}>
+                <li className={spotsOpen ? 'navbar--selected--item' : 'navbar--unselected--item'} onMouseEnter={handleSpotsClick} onMouseLeave={handleLeave}>
                     {spotsOpen ? 
                         <img src={spotsIconWhite} alt='Spots Icon'/> 
                         : 
                         <img src={spotsIconBlack} alt='Spots Icon'/>}
                     Spots
-                </li>
-                {spotsOpen ?                                        // Render Spots if is open
-                    <div className='navbar--dropdown--spots'>
+                    {spotsOpen ?                                        // Render Spots if is open
+                    <div className='navbar--dropdown--spots' style={{ top: `${dropdownY}px`, left: `${dropdownX}px`, color: 'black'}}>
                         {spots.map((spot) => (
                             spot == capitalizeAfterUnderscore(props.currSpot) ? 
                             <li onClick={handleSpotClick} className='selected--spot'>{spot}</li> :
@@ -101,19 +111,21 @@ export default function GeneralNavbar(props) {
                         ))}
                     </div> : null
                 }
+                </li>
                 
-                <li className={camsOpen ? 'navbar--selected--item' : 'navbar--unselected--item'} onClick={handleCamsClick}>
+                <li className={camsOpen ? 'navbar--selected--item' : 'navbar--unselected--item'} onMouseEnter={handleCamsClick} onMouseLeave={handleLeave}>
                     {camsOpen ? 
                         <img src={cameraIconWhite} alt='Camera Icon'/>
                         :
                         <img src={cameraIconBlack} alt='Camera Icon'/>
                         }
-                    Cams</li>
-                {camsOpen ?                                        // Render Cams if is open
-                    <div className='navbar--dropdown--cams'>
+                    Cams
+                    {camsOpen ?                                        // Render Cams if is open
+                    <div className='navbar--dropdown--cams' style={{ top: `${dropdownY}px`, left: `${dropdownX}px`, color: 'black'}}>
                         <li>Coming soon!</li>
                     </div> : null
                 }
+                    </li>
 
                 <li className='navbar--unselected--item'>
                     <img src={aboutIconBlack} alt='About Icon'/>
@@ -129,15 +141,15 @@ export default function GeneralNavbar(props) {
                     <img src={githubIconBlack} alt='Github Icon'/>
                     Contribute
                 </li>
+            </ul>
 
                 <div className='general--navbar--logout'>
-                    <img src={navbarLine} />
                     <li onClick={handleLogout}>
                         <img src={logoutIcon} alt='Logout Icon'/>
                         Logout
                     </li>
                 </div>
-            </ul>
+            
 
         </div>
     )
