8b9365e1813f4bcff246ded0f39546d0070459bd
Sean Muirhead <smmuirhead100@berkeley.edu>
2023-09-12 11:30:08 -0700

    add footer and login to home page

---
+++ frontend/app/src/components/tsFooter.tsx
@@ -0,0 +1,13 @@
+export default function TSFooter() {
+    return (
+        <div className="flex flex-col gap-5 m-5">
+            <div className="border-solid border-2 border-transparent border-b-solid border-b-3 border-b-gray-400 "></div>
+            <ul className="flex justify-evenly text-gray-600 text-sm">
+                <li>Home</li>
+                <li>Contact</li>
+                <li>About</li>
+                <li>FAQ</li>
+            </ul>
+        </div>
+    )
+}
--- frontend/app/src/pages/home/home.tsx
+++ frontend/app/src/pages/home/home.tsx
@@ -3,39 +3,37 @@ import logo from '../../assets/logo.svg'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import homeImage from '../../assets/homeImage.jpg'
+import login from '../../assets/login.svg'
 import exampleDashboard from '../../assets/exampleDashboard.png'
 import { useAuth } from '../../context/AuthProvider'
+import TSFooter from '../../components/tsFooter.tsx'
 
 export default function Home() {
     let navigate = useNavigate()
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
     const auth = (useAuth()['auth'])
     
     function handleSignUp(){
         let path = '/signup'
         navigate(path)
     }
-    
-    useEffect(() => {
-        function handleResize() {
-          setWindowWidth(window.innerWidth);
-        }
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-      }, [windowWidth]);
 
     function handleSignIn(){
         let path='/signin'
         navigate(path)
     }
+
     if (auth) {
         navigate('/forecast')
     } else {
         return (
             <div className="flex flex-col">
-                <div className="flex p-2 bg-white justify-center sticky top-0">
+                <div className="flex p-2 bg-white justify-between sticky top-0">
                     <div>
                         <img src={logo} className="w-10"/>
+                    </div>
+                    <div className='flex items-center gap-1 text-gray-600 px-2' onClick={handleSignIn}>
+                        <div>Login</div>
+                        <img src={login} className="w-3" />
                     </div>
                 </div>
                 <div className="bg-cover bg-center bg-no-repeat h-80 flex justify-center items-center flex-col gap-6"style={{backgroundImage: `url(${homeImage})`}}>
@@ -51,10 +49,12 @@ export default function Home() {
                 <div className='flex flex-col gap-10 py-10 items-center my-3'>
                     <div className="text-center px-10 text-gray-600">Get access to free 16 day forecasts including wind, tide, and height information.</div>
                     <div className="border-solid border-2 border-transparent border-b-solid border-b-3 border-b-black w-1/2"></div>
-                    <img src={exampleDashboard} className="width-30 my-5"/>
+                    <img src={exampleDashboard} className="w-2/3 my-5"/>
                     <div className="text-center px-10 text-gray-600">As an early user, we’ll give you the ability to request the addition of new spots!</div>
-                    <div className="bg-twosurfblue py-2 px-3 text-white font-bold rounded-lg hover:cursor-default" onClick={handleSignIn}>Get started</div>
+                    <button className="bg-twosurfblue border-solid border-2 border-transparent hover:border-black hover:border-opacity-20 py-2 px-3 text-white font-bold rounded-lg hover:cursor-default" onClick={handleSignUp}>Get started</button>
                 </div>
+
+                <TSFooter />
             </div>
     )
         }
